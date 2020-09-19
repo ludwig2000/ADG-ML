5a2b52b9df2e860be2273a96cd2c336d5b36a83d
ritwik <ritwik@localhost.localdomain>
2020-09-20 03:53:35 +0530

    update

--- WEEK-2/logistic_regression/logistic_regression.py
+++ WEEK-2/logistic_regression/logistic_regression.py
@@ -11,7 +11,7 @@ x1=np.array(x)
 X=x1.reshape(-1,1)
 y=bc.target
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.4,random_state=42)
 
 def accuracy(y_true,y_pred):
     accuracy=np.sum(y_true==y_pred)/len(y_true)
