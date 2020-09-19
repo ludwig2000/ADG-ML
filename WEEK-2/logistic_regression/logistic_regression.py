ff36c78b30f5896dc4616307bb3bbeef2e48a5ca
ritwik <ritwik@localhost.localdomain>
2020-09-20 03:35:58 +0530

    update

--- WEEK-2/logistic_regression/logistic_regression.py
+++ WEEK-2/logistic_regression/logistic_regression.py
@@ -2,11 +2,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
-
+import pandas as pd
 from logistic_regression_tests import LogisticRegression
 
-bc=datasets.load_breast_cancer()
-X,y=bc.data,bc.target
+bc=pd.read_csv('/home/ritwik/Desktop/Workspace/ADG-ML/WEEK-2/heart.csv')
+x=bc["chol"]
+x1=np.array(x)
+X=x1.reshape(-1,1)
+y=bc.target
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
 
