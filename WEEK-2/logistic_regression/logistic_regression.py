31fd49c6771e16a3222f954353f1e6078d53f1f5
ritwik <ritwik@localhost.localdomain>
2020-09-20 03:09:16 +0530

    Update

---
+++ WEEK-2/logistic_regression/logistic_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+from logistic_regression_tests import LogisticRegression
+
+bc=datasets.load_breast_cancer()
+X,y=bc.data,bc.target
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
+
+def accuracy(y_true,y_pred):
+    accuracy=np.sum(y_true==y_pred)/len(y_true)
+    return accuracy
+
+reg=LogisticRegression(lr=0.0001,n_iters=5000)
+reg.fit(X_train,y_train)
+predictions=reg.predict(X_test)
+
+print("LR accuracy: ",accuracy(y_test,predictions))
