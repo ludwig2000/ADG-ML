4a7348f6812859ce1f3439f576654c3fff273c79
ritwik <ritwik@localhost.localdomain>
2020-12-06 20:18:18 +0530

    u

--- WEEK-2/logistic_regression/logistic_regression.py
+++ WEEK-2/logistic_regression/logistic_regression.py
@@ -1,7 +1,5 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import datasets
-import matplotlib.pyplot as plt
 import pandas as pd
 from logistic_regression_tests import LogisticRegression
 
